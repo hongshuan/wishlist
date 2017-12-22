c05a97ea379a6e769e316fc9526eb58b4366505f
Hanson Li <lihsca@gmail.com>
2017-12-22 11:41:27 -0500

    PurchaseOrderResult & PriceAvailResult

--- suppliers/common/interface.go
+++ suppliers/common/interface.go
@@ -1,11 +1,36 @@
 package common
 
-type PriceAvailResult struct {
-}
+const STATUS_OK    = "OK"
+const STATUS_ERROR = "ERR"
 
 type SupplierClient interface {
     GetPrefix() string
     GetPriceAvail(sku string)
     PurchaseOrder(info string)
     DropshipOrder(info string)
+}
+
+type PriceAvailBranch struct {
+    Name     string
+    Code     string
+    Qty      string
+}
+
+type PriceAvailItem struct {
+    Sku      string
+    Status   string
+    Price    string
+    Branches []PriceAvailBranch
+}
+
+type PriceAvailResult struct {
+    Status       string
+    ErrorMessage string
+    Items        []PriceAvailItem
+}
+
+type PurchaseOrderResult struct {
+    Status       string
+    OrderNo      string
+    ErrorMessage string
 }
