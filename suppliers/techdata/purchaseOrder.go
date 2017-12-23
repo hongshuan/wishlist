33f4f8314e5d9ddf1de30e9b001dd2535d2bf994
Hongshuan Li <lihsca@gmail.com>
2017-12-22 22:48:21 -0500

    TD purchaseOrder Request/Response

--- suppliers/techdata/purchaseOrder.go
+++ suppliers/techdata/purchaseOrder.go
@@ -79,6 +79,76 @@ import (
 
 type PurchaseOrderRequest struct {
     XMLName     xml.Name `xml:"XML_Order_Submit"`
+    Header struct {
+        UserName        string `xml:"UserName"`
+        Password        string `xml:"Password"`
+        ResponseVersion string `xml:"ResponseVersion"`
+        OrderTypeCode   string `xml:"OrderTypeCode"`
+        PONbr           string `xml:"PONbr"`
+
+        RefNbrs struct {
+            RefIDQual string `xml:"RefIDQual"`
+            RefID     string `xml:"RefID"`
+        } `xml:"RefNbrs"`
+
+        SalesRequirementCode string `xml:"SalesRequirementCode"`
+        Name                 string `xml:"Name"`
+        AddrInfo           []string `xml:"AddrInfo>Addr"`
+        CityName             string `xml:"CityName"`
+        StateProvinceCode    string `xml:"StateProvinceCode"`
+        PostalCode           string `xml:"PostalCode"`
+        ContactName          string `xml:"ContactName"`
+        ContactPhoneNbr      string `xml:"ContactPhoneNbr"`
+
+        EndUserInfo struct {
+            GovAgency         string `xml:"EuiGovAgency"`
+            GovCabinetLevel   string `xml:"EuiGovCabinetLevel"`
+            ContractNbr       string `xml:"EuiContractNbr"`
+            ContractType      string `xml:"EuiContractType"`
+            OrderPriority     string `xml:"EuiOrderPriority"`
+            MarketType        string `xml:"EuiMarketType"`
+            ContactName       string `xml:"EuiContactName"`
+            PhoneNbr          string `xml:"EuiPhoneNbr"`
+            FaxNbr            string `xml:"EuiFaxNbr"`
+            Name              string `xml:"EuiName"`
+            Addr1             string `xml:"EuiAddr1"`
+            Addr2             string `xml:"EuiAddr2"`
+            Addr3             string `xml:"EuiAddr3"`
+            CityName          string `xml:"EuiCityName"`
+            StateProvinceCode string `xml:"EuiStateProvinceCode"`
+            PostalCode        string `xml:"EuiPostalCode"`
+            CountryCode       string `xml:"EuiCountryCode"`
+            SicCode           string `xml:"EuiSicCode"`
+            OrderPromoType    string `xml:"EuiOrderPromoType"`
+            EndUserLicenseNbr string `xml:"EuiEndUserLicenseNbr"`
+            EndUserPODate     string `xml:"EuiEndUserPODate"`
+            EndUserRef1       string `xml:"EuiEndUserRef1"`
+            EndUserRef2       string `xml:"EuiEndUserRef2"`
+            EndUserRef3       string `xml:"EuiEndUserRef3"`
+            InstallName       string `xml:"EuiInstallName"`
+            DropShipType      string `xml:"EuiDropShipType"`
+            ContactEmailAddr1 string `xml:"EuiContactEmailAddr1"`
+            ContactEmailAddr2 string `xml:"EuiContactEmailAddr2"`
+        } `xml:"EndUserInfo"`
+
+        MyOrderTracker struct {
+            ResellerEmail  string `xml:"ResellerEmail"`
+            ResellerEvents string `xml:"ResellerEvents"`
+            ResellerEvents string `xml:"ResellerEvents"`
+        } `xml:"MyOrderTracker"`
+
+    } `xml:"Header"`
+
+    Detail struct {
+        LineInfo struct {
+            QtyOrdered       string `xml:"QtyOrdered"`
+            ProductIDQual    string `xml:"ProductIDQual"`
+            ProductID        string `xml:"ProductID"`
+            WhseCode         string `xml:"WhseCode"`
+            IDCode           string `xml:"IDCode"`
+            OrderMessageLine string `xml:"OrderMessageLine"`
+        } `xml:"LineInfo"`
+    } `xml:"Detail"`
 }
 
 /**
@@ -99,4 +169,16 @@ type PurchaseOrderRequest struct {
 
 type PurchaseOrderResponse struct {
     XMLName     xml.Name `xml:"XML_Order_Response"`
+
+    Header struct {
+        Username        string `xml:"UserName"`
+        TransSetIDCode  string `xml:"TransSetIDCode "`
+        TransControlID  string `xml:"TransControlID "`
+        ResponseVersion string `xml:"ResponseVersion "`
+        PurposeCode     string `xml:"PurposeCode "`
+        PoNum           string `xml:"PONbr"`
+        RefID           string `xml:"RefID"`
+    } `xml:"Header"`
+
+    Summary string `xml:"Summary"`
 }
