02b071862c4cddd71f4683076d35f1ecacc93fdb
Hanson Li <lihsca@gmail.com>
2018-01-04 16:42:32 -0500

    str2bytes & bytes2str

---
+++ tmp/misc/str2bytes.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unsafe"
+)
+
+func str2bytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func bytes2str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
